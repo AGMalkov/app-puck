62db98e273a62f139a5b78e82fc50ef88691b16a
AGMalkov <malkov95@yandex.ru>
2024-12-07 11:24:28 +0500

    Add files via upload

--- app/modules/user.py
+++ app/modules/user.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import relationship
 
 class User(Base):
     __tablename__ = 'users'
+
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String)
     firstname = Column(String)
